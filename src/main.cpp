eb13fb0c0008ea1188cec1529812f422bca02561
dkadyrov <dizzarg@mail.ru>
2024-07-23 14:01:28 +0300

    glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
+#include <cstdio>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main()
 {
-    GLFWwindow* window{};
-
     /* Initialize the library */
     if (!glfwInit()) {
         std::cout << "Init error" << std::endl;
         return -1;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    auto const window = glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
     if (!window)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -26,19 +25,29 @@ int main()
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) <<std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) <<std::endl;
+    std::cout << "Version: " << glGetString(GL_VERSION) <<std::endl;
+    std::cout << "Shading language version : " << glGetString(GL_SHADING_LANGUAGE_VERSION) <<std::endl;
+
     glViewport(0, 0, 800, 600);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        float time = glfwGetTime();
+        glClearColor(abs(sin(time)), abs(cos(time)), abs(sin(time + 3.14f/4.0f)), 1.0);
+
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
